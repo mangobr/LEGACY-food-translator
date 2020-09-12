de266d4bbb65499675b2d9ef1b5b9fc1001ab290
Jeremias Moraes <moraesjeremias@gmail.com>
2020-09-12 20:24:27 -0300

    Creating a json write stream

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-const TJO = require("translate-json-object");
-const dotenv = require('dotenv').config();
+require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
 const { simpleTranslateMock } = require("./tests/__mocks__/mocks.js");
-
-
+const TJO = require("translate-json-object");
 const translator = TJO();
 
 translator.init({
   googleApiKey: process.env.GOOGLE_CLOUD_API,
 });
 
-
-  translator.translate(simpleTranslateMock(), "en")
+translator
+  .translate(simpleTranslateMock(), "en")
   .then((data) => {
-    console.log(data)
+    const writeStream = fs.createWriteStream(
+      path.resolve(__dirname, "json", "exports", "translate.json")
+    );
+    writeStream.write(JSON.stringify(data), (error) => {
+      if (error) {
+        console.log("Error: ", error.message);
+        throw new Error();
+      }
+    });
   })
   .catch((error) => {
-    console.log('Error: ', error)
-  })
-  
-
+    console.log("Error: ", error);
+  });
